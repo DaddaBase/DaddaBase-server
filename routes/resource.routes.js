9f6563400ffb532c5ac997f42f08449b46f13de7
Lai In Chang <lavender880331@gmail.com>
2023-01-25 16:52:30 +0100

    can post new resource and post

--- routes/resource.routes.js
+++ routes/resource.routes.js
@@ -3,16 +3,12 @@ const router = require("express").Router();
 // const mongoose = require('mongoose');
  
 const Resource = require('../models/Resource.model');
-const User = require('../models/User.model');
  
 //  POST /api/resources  -  Creates a new resource
 router.post('/resources', (req, res, next) => {
-  const { title, description, url, userId } = req.body;
+  const { title, description, url } = req.body;
 
-  Resource.create({ title, description, url, user: userId })
-    .then(newResource => {
-      return User.findByIdAndUpdate(userId, { $push: { resources: newResource._id }})
-    })
+  Resource.create({ title, description, url /*, user: {}*/ })
     .then(response => {
         console.log("create resource success");
         res.json(response)})
