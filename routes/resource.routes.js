79ec7ea32162f89ada1e337c7863bee02c9f7639
Lai In Chang <lavender880331@gmail.com>
2023-01-25 17:17:26 +0100

    link resource to user

--- routes/resource.routes.js
+++ routes/resource.routes.js
@@ -3,12 +3,16 @@ const router = require("express").Router();
 // const mongoose = require('mongoose');
  
 const Resource = require('../models/Resource.model');
+const User = require('../models/User.model');
  
 //  POST /api/resources  -  Creates a new resource
 router.post('/resources', (req, res, next) => {
-  const { title, description, url } = req.body;
+  const { title, description, url, userId } = req.body;
 
-  Resource.create({ title, description, url /*, user: {}*/ })
+  Resource.create({ title, description, url, user: userId })
+    .then(newResource => {
+      return User.findByIdAndUpdate(userId, { $push: { resources: newResource._id }})
+    })
     .then(response => {
         console.log("create resource success");
         res.json(response)})
