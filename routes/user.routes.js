4255c3118b00f10b67af12a0ef70075c720e5ef4
Lai In Chang <lavender880331@gmail.com>
2023-02-01 13:06:00 +0100

    add Cloudinary function

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,22 @@
-const router = require("express").Router();
- 
+const router = require("express").Router(); 
 const mongoose = require('mongoose');
- 
 const User = require('../models/User.model');
+const fileUploader = require("../config/cloudinary.config");
+
+// POST "/api/users/:userId/upload" => Route that receives the image, sends it to Cloudinary via the fileUploader and returns the image URL
+router.put("/users/:userId/upload", fileUploader.single("profileImage"), (req, res, next) => {
+  // console.log("file is: ", req.file)
+ 
+  if (!req.file) {
+    next(new Error("No file uploaded!"));
+    return;
+  }
+
+  // Get the URL of the uploaded file and send it as a response.
+  // 'fileUrl' can be any name, just make sure you remember to use the same when accessing it on the frontend
+  
+  res.json({ secure_url: req.file.path });
+});
 
 //  GET /api/users/:userId -  Retrieves a specific user by id
 router.get('/users/:userId', (req, res, next) => {
